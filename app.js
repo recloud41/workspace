fb7b4da60c61def25622877a18567465a3a7f1e9
recloud41 <satyarekapalli41@gmail.com>
2024-10-18 07:25:49 +0000

    chnge

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Bolds from Satya Node.js App!');
+  res.send('Hello Bolds from Satyeahs Node.js App!');
 });
 
 app.listen(PORT, () => {
